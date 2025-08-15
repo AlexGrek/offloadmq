a02cd27bebf4e42d4417e1df46f9d324ddc83424
Alex Grek <alexgrek.hq@gmail.com>
2025-08-15 15:39:29 +0300

    List tasks backend

--- src/api/mgmt/mod.rs
+++ src/api/mgmt/mod.rs
@@ -79,9 +79,29 @@ pub async fn revoke_client_api_key(
 
 pub async fn add_client_api_key(
     State(state): State<Arc<AppState>>,
-    Json(new_key): Json<schema::CreateApiKeyRequest>
+    Json(new_key): Json<schema::CreateApiKeyRequest>,
 ) -> Result<impl IntoResponse, AppError> {
     let key: ClientApiKey = new_key.into();
     state.storage.client_keys.upsert_key(&key.key, &key)?;
     Ok(Json(key))
+}
+
+pub async fn list_tasks(State(state): State<Arc<AppState>>) -> Result<impl IntoResponse, AppError> {
+    let tasks = state.urgent.tasks.blocking_read();
+    let urgent: Vec<_> = tasks.iter().map(|entry| entry.1).collect();
+    let urgent_assigned: Vec<_> = urgent
+        .iter()
+        .filter_map(|entry| entry.assigned_task.clone())
+        .collect();
+    let urgent_unassigned: Vec<_> = urgent
+        .iter()
+        .filter(|entry| entry.assigned_task.is_none())
+        .map(|entry| entry.task.clone())
+        .collect();
+    let regular_assigned = state.storage.tasks.list_assigned_all()?;
+    let regular_unassigned = state.storage.tasks.list_unassigned_all()?;
+    Ok(Json(json!({"urgent": {"assigned": urgent_assigned,
+                                "unassigned": urgent_unassigned},
+                            "regular": {"assigned": regular_assigned,
+                                "unassigned": regular_unassigned}})))
 }
